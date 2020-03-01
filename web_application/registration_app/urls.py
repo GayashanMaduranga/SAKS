bf9c235d4c49b21910aca69ff51123d2e611a289
gayashan maduranga <gmkariyawasam@gmail.com>
2020-03-01 21:32:42 +0530

    adds server side validations

--- web_application/registration_app/urls.py
+++ web_application/registration_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from registration_app import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='registration'),
 ]
--- web_application/registration_app/views.py
+++ web_application/registration_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.template.context_processors import csrf
 from registration_app.models import User
 
 # Create your views here.
 def index(request):
+    context = {}
+    context.update(csrf(request))
+
     if request.method == 'POST':
         full_name = request.POST['full_name']
         email = request.POST['email']
@@ -14,13 +17,24 @@ def index(request):
         permanent_address = request.POST['permanent_address']
         nid_number = request.POST['nid_number']
 
+        if User.objects.filter(user_name=user_name).exists():
+            messages.error(request,'User Name is Already Taken')
+            return redirect('registration')
+            
+        if User.objects.filter(nid_number=nid_number).exists():
+            messages.error(request,'nid number is Already registered')
+            return redirect('registration')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request,'email is Already registered')
+            return redirect('registration')
+
         user = User.objects.get_or_create(full_name=full_name,email=email,user_name=user_name,
         phone_number=phone_number,present_address=present_address,permanent_address=permanent_address,
         nid_number=nid_number)
         # user.save()
-        print('method is posted' + full_name)
-        messages.error(request,'TESTing Error message !!!!')
+        # print('method is posted' + full_name)
+        messages.info(request,'You have successfully registered')
 
-    context = {}
-    context.update(csrf(request))
+
     return render(request,'registration_app/index.html',context)
